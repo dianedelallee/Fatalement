d0a826033fa341a22bcb51550cc263fa0b5cb681
Diane Delallée <diane.delallee@pix4d.com>
2018-08-09 16:17:23 +0200

    feat(): adding pagination to list of articles

--- website/views.py
+++ website/views.py
@@ -1,11 +1,20 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 
 from website.models.article import Article
 
+
 def home(request):
+    articles_list = Article.objects.all().order_by('-date', '-id')
+    page = request.GET.get('page', 1)
+    paginator = Paginator(articles_list, 10)
 
-    articles = Article.objects.all().order_by('-date', '-id')
-
+    try:
+        articles = paginator.page(page)
+    except PageNotAnInteger:
+        articles = paginator.page(1)
+    except EmptyPage:
+        articles = paginator.page(paginator.num_pages)
 
     return render(
         request, 'website/home.html',
